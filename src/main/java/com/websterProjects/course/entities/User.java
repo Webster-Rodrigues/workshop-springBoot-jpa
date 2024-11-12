babbf3cb22190f08ae9988f84b88f076d7bbef10
Webster Diogenes <web.driw12@gmail.com>
2024-11-12 15:03:31 -0300

    H2 database, test profile, JPA

--- src/main/java/com/websterProjects/course/entities/User.java
+++ src/main/java/com/websterProjects/course/entities/User.java
@@ -2,10 +2,19 @@ package com.websterProjects.course.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user") //Renomeando a tabela porque a palavra "User" é reservada no H2
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Torna o Id auto increment
 	private Long id;
 	private String name;
 	private String email;
