dcf106ba84b24c4dc4974d289d6ed6922ad13940
Webster Diogenes <web.driw12@gmail.com>
2024-12-07 14:23:58 -0300

    Exception handling - delete

--- src/main/java/com/websterProjects/course/resources/exeptions/ResourceExceptionHandler.java
+++ src/main/java/com/websterProjects/course/resources/exeptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.websterProjects.course.services.exceptions.DatabaseException;
 import com.websterProjects.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +20,15 @@ public class ResourceExceptionHandler {
 		
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)  //Torna o método capaz de interceptar a exceção. Vai interceptar qualquer exceção do tipo ResourceNotFoundException e fará o tratamento necessário 
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		
+		String error = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
