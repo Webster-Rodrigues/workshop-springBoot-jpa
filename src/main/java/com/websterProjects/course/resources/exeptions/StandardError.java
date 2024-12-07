87e4ff362af7c418e23535f4e3ece2d7fb24db61
Webster Diogenes <web.driw12@gmail.com>
2024-12-07 14:08:30 -0300

    Exception handling - findById

---
+++ src/main/java/com/websterProjects/course/resources/exeptions/StandardError.java
@@ -0,0 +1,87 @@
+package com.websterProjects.course.resources.exeptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+public class StandardError implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-mm-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+	private Instant timestap;
+	private Integer status;
+	private String erro;
+	private String message;
+	private String path;
+	
+	
+	public StandardError() {
+		
+	}
+
+
+	public StandardError(Instant timestap, Integer status, String erro, String message, String path) {
+		this.timestap = timestap;
+		this.status = status;
+		this.erro = erro;
+		this.message = message;
+		this.path = path;
+	}
+
+
+	public Instant getTimestap() {
+		return timestap;
+	}
+
+
+	public void setTimestap(Instant timestap) {
+		this.timestap = timestap;
+	}
+
+
+	public Integer getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+
+	public String getErro() {
+		return erro;
+	}
+
+
+	public void setErro(String erro) {
+		this.erro = erro;
+	}
+
+
+	public String getMessage() {
+		return message;
+	}
+
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+
+	public String getPath() {
+		return path;
+	}
+
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+	
+	
+	
+	
+}
--- src/main/java/com/websterProjects/course/services/UserService.java
+++ src/main/java/com/websterProjects/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.websterProjects.course.entities.User;
 import com.websterProjects.course.repositories.UserRepository;
+import com.websterProjects.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); //Tenta o get se não tiver lança a exceção ResourceNotFoundException
 	}
 	
 	public User insert(User obj) {
